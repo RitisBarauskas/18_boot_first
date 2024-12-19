20e0be2cb9377d5ed765d76684a088af204047e0
Ritis Barauskas <perm300ru@yandex.ru>
2024-12-19 20:53:15 +0300

    Added new name Savva

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from random import choice
 
-NAMES = ['Alice', 'Bob', 'Charlie', 'David', 'Eve', 'Frank', 'Grace', 'Heidi', 'Ivan', 'Judy']
+NAMES = ['Alice', 'Bob', 'Charlie', 'David', 'Eve', 'Frank', 'Савва', 'Heidi', 'Ivan', 'Judy']
 
 for _ in range(20):
     name = choice(NAMES)
